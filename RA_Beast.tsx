758158381bd7d4d8deb8c160c47a58f760120234
Muhammad Afuzarahman <theafuza@gmail.com>
2024-09-08 21:31:40 -0700

    Add dependency tilesets saved as Tiled files

---
+++ RA_Beast.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="RA_Beast" tilewidth="16" tileheight="16" tilecount="448" columns="28">
+ <image source="../../Assets/Rogue Adventure World 2.9.0/Rogue Adventure World 2.9.0/Tilesets (16x16)/RA_Beast.png" width="448" height="256"/>
+</tileset>
