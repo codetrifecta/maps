fe859ca76436f0fa96140ba6825e6828f28a9444
Denise Tan <denisetanumihardja@yahoo.com>
2024-09-19 23:45:14 -0700

    Designed Stage 1, Floor 1, Common rooms ver. 1 and 2

--- RA_Hell.tsx
+++ RA_Hell.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="RA_Hell" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="../../Assets/Rogue Adventure World 2.9.0/Rogue Adventure World 2.9.0/Tilesets (16x16)/RA_Hell.png" width="256" height="256"/>
+ <image source="tilesets/RA_Hell.png" width="256" height="256"/>
 </tileset>
